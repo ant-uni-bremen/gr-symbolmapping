cc47749a280a4306408778607f8164000af5fffc
Johannes Demel <demel@ant.uni-bremen.de>
2020-03-19 15:48:09 +0100

    grc: Add grc files and demo flowgraphs

--- examples/symbol_mapping_demo.py
+++ examples/symbol_mapping_demo.py
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: GPL-3.0
 #
 # GNU Radio Python Flow Graph
-# Title: Interleaver Demo
+# Title: Symbol Mapping Demo
 # Author: Johannes Demel
 # GNU Radio version: 3.9.0.0-git
 
@@ -27,22 +27,22 @@ from gnuradio.filter import firdes
 import sip
 from gnuradio import analog
 from gnuradio import blocks
+from gnuradio import digital
 from gnuradio import gr
 import sys
 import signal
 from argparse import ArgumentParser
 from gnuradio.eng_arg import eng_float, intx
 from gnuradio import eng_notation
-import numpy as np
 import symbolmapping
 from gnuradio import qtgui
 
-class interleaver_demo(gr.top_block, Qt.QWidget):
+class symbol_mapping_demo(gr.top_block, Qt.QWidget):
 
     def __init__(self):
-        gr.top_block.__init__(self, "Interleaver Demo", catch_exceptions=True)
+        gr.top_block.__init__(self, "Symbol Mapping Demo", catch_exceptions=True)
         Qt.QWidget.__init__(self)
-        self.setWindowTitle("Interleaver Demo")
+        self.setWindowTitle("Symbol Mapping Demo")
         qtgui.util.check_set_qss()
         try:
             self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
@@ -60,7 +60,7 @@ class interleaver_demo(gr.top_block, Qt.QWidget):
         self.top_grid_layout = Qt.QGridLayout()
         self.top_layout.addLayout(self.top_grid_layout)
 
-        self.settings = Qt.QSettings("GNU Radio", "interleaver_demo")
+        self.settings = Qt.QSettings("GNU Radio", "symbol_mapping_demo")
 
         try:
             if StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
@@ -74,13 +74,12 @@ class interleaver_demo(gr.top_block, Qt.QWidget):
         # Variables
         ##################################################
         self.samp_rate = samp_rate = 32000
-        self.intl_indices = intl_indices = np.random.permutation(160)
 
         ##################################################
         # Blocks
         ##################################################
-        self.symbolmapping_interleaver_0_0 = symbolmapping.interleaver_ff(intl_indices, 1 > 0, False)
-        self.symbolmapping_interleaver_0 = symbolmapping.interleaver_ff(intl_indices, 1 > 0, True)
+        self.symbolmapping_symbol_mapper_bc_0 = symbolmapping.symbol_mapper_bc(4, 'GRAY', True)
+        self.symbolmapping_symbol_demapper_cf_0 = symbolmapping.symbol_demapper_cf(4, 'GRAY')
         self.qtgui_time_sink_x_0 = qtgui.time_sink_f(
             1024, #size
             samp_rate, #samp_rate
@@ -88,7 +87,7 @@ class interleaver_demo(gr.top_block, Qt.QWidget):
             2 #number of inputs
         )
         self.qtgui_time_sink_x_0.set_update_time(0.10)
-        self.qtgui_time_sink_x_0.set_y_axis(-1, 1)
+        self.qtgui_time_sink_x_0.set_y_axis(-1, 260)
 
         self.qtgui_time_sink_x_0.set_y_label('Amplitude', "")
 
@@ -128,22 +127,77 @@ class interleaver_demo(gr.top_block, Qt.QWidget):
 
         self._qtgui_time_sink_x_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0.pyqwidget(), Qt.QWidget)
         self.top_grid_layout.addWidget(self._qtgui_time_sink_x_0_win)
-        self.blocks_throttle_0 = blocks.throttle(gr.sizeof_float*1, samp_rate,True)
-        self.analog_fastnoise_source_x_0 = analog.fastnoise_source_f(analog.GR_GAUSSIAN, 1, 0, 8192)
+        self.qtgui_const_sink_x_0 = qtgui.const_sink_c(
+            1024, #size
+            "", #name
+            1 #number of inputs
+        )
+        self.qtgui_const_sink_x_0.set_update_time(0.10)
+        self.qtgui_const_sink_x_0.set_y_axis(-2, 2)
+        self.qtgui_const_sink_x_0.set_x_axis(-2, 2)
+        self.qtgui_const_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 0, "")
+        self.qtgui_const_sink_x_0.enable_autoscale(False)
+        self.qtgui_const_sink_x_0.enable_grid(False)
+        self.qtgui_const_sink_x_0.enable_axis_labels(True)
+
+
+        labels = ['', '', '', '', '',
+            '', '', '', '', '']
+        widths = [1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1]
+        colors = ["blue", "red", "red", "red", "red",
+            "red", "red", "red", "red", "red"]
+        styles = [0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0]
+        markers = [0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0]
+        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
+            1.0, 1.0, 1.0, 1.0, 1.0]
+
+        for i in range(1):
+            if len(labels[i]) == 0:
+                self.qtgui_const_sink_x_0.set_line_label(i, "Data {0}".format(i))
+            else:
+                self.qtgui_const_sink_x_0.set_line_label(i, labels[i])
+            self.qtgui_const_sink_x_0.set_line_width(i, widths[i])
+            self.qtgui_const_sink_x_0.set_line_color(i, colors[i])
+            self.qtgui_const_sink_x_0.set_line_style(i, styles[i])
+            self.qtgui_const_sink_x_0.set_line_marker(i, markers[i])
+            self.qtgui_const_sink_x_0.set_line_alpha(i, alphas[i])
+
+        self._qtgui_const_sink_x_0_win = sip.wrapinstance(self.qtgui_const_sink_x_0.pyqwidget(), Qt.QWidget)
+        self.top_grid_layout.addWidget(self._qtgui_const_sink_x_0_win)
+        self.digital_binary_slicer_fb_0 = digital.binary_slicer_fb()
+        self.blocks_unpacked_to_packed_xx_0 = blocks.unpacked_to_packed_bb(1, gr.GR_MSB_FIRST)
+        self.blocks_uchar_to_float_0_0 = blocks.uchar_to_float()
+        self.blocks_uchar_to_float_0 = blocks.uchar_to_float()
+        self.blocks_throttle_0 = blocks.throttle(gr.sizeof_char*1, samp_rate,True)
+        self.blocks_float_to_char_0 = blocks.float_to_char(1, 1)
+        self.blocks_char_to_float_0_0 = blocks.char_to_float(1, -1)
+        self.blocks_add_const_vxx_0 = blocks.add_const_ff(1)
+        self.analog_random_uniform_source_x_0 = analog.random_uniform_source_b(0, 256, 0)
 
 
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.analog_fastnoise_source_x_0, 0), (self.blocks_throttle_0, 0))
-        self.connect((self.blocks_throttle_0, 0), (self.qtgui_time_sink_x_0, 1))
-        self.connect((self.blocks_throttle_0, 0), (self.symbolmapping_interleaver_0, 0))
-        self.connect((self.symbolmapping_interleaver_0, 0), (self.symbolmapping_interleaver_0_0, 0))
-        self.connect((self.symbolmapping_interleaver_0_0, 0), (self.qtgui_time_sink_x_0, 0))
+        self.connect((self.analog_random_uniform_source_x_0, 0), (self.blocks_throttle_0, 0))
+        self.connect((self.blocks_add_const_vxx_0, 0), (self.blocks_float_to_char_0, 0))
+        self.connect((self.blocks_char_to_float_0_0, 0), (self.blocks_add_const_vxx_0, 0))
+        self.connect((self.blocks_float_to_char_0, 0), (self.blocks_unpacked_to_packed_xx_0, 0))
+        self.connect((self.blocks_throttle_0, 0), (self.blocks_uchar_to_float_0, 0))
+        self.connect((self.blocks_throttle_0, 0), (self.symbolmapping_symbol_mapper_bc_0, 0))
+        self.connect((self.blocks_uchar_to_float_0, 0), (self.qtgui_time_sink_x_0, 0))
+        self.connect((self.blocks_uchar_to_float_0_0, 0), (self.qtgui_time_sink_x_0, 1))
+        self.connect((self.blocks_unpacked_to_packed_xx_0, 0), (self.blocks_uchar_to_float_0_0, 0))
+        self.connect((self.digital_binary_slicer_fb_0, 0), (self.blocks_char_to_float_0_0, 0))
+        self.connect((self.symbolmapping_symbol_demapper_cf_0, 0), (self.digital_binary_slicer_fb_0, 0))
+        self.connect((self.symbolmapping_symbol_mapper_bc_0, 0), (self.qtgui_const_sink_x_0, 0))
+        self.connect((self.symbolmapping_symbol_mapper_bc_0, 0), (self.symbolmapping_symbol_demapper_cf_0, 0))
 
     def closeEvent(self, event):
-        self.settings = Qt.QSettings("GNU Radio", "interleaver_demo")
+        self.settings = Qt.QSettings("GNU Radio", "symbol_mapping_demo")
         self.settings.setValue("geometry", self.saveGeometry())
         event.accept()
 
@@ -155,15 +209,9 @@ class interleaver_demo(gr.top_block, Qt.QWidget):
         self.blocks_throttle_0.set_sample_rate(self.samp_rate)
         self.qtgui_time_sink_x_0.set_samp_rate(self.samp_rate)
 
-    def get_intl_indices(self):
-        return self.intl_indices
-
-    def set_intl_indices(self, intl_indices):
-        self.intl_indices = intl_indices
 
 
-
-def main(top_block_cls=interleaver_demo, options=None):
+def main(top_block_cls=symbol_mapping_demo, options=None):
 
     if StrictVersion("4.5.0") <= StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
         style = gr.prefs().get_string('qtgui', 'style', 'raster')
